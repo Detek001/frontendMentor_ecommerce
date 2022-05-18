65eade276b7e6219baff30d547b4aaa1892947f7
detek001 <dennytekne@icloud.com>
2022-05-18 21:52:18 +0200

    js for counterbutton finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import { init } from "./src/script/init.js"
 
 const onload = (callback) => {
--- src/script/registerCounterButton.js
+++ src/script/registerCounterButton.js
@@ -1,6 +1,35 @@
 export const registerCounterButton = () => {
-
-  const getItems = () => {
-    
+  
+  const getItems = (datattr) => {
+    return document.querySelectorAll(datattr)
   }
+
+  const executeOperation = (operation) => {
+    let number = document.querySelector(".number")
+    let currentValue = parseInt(number.textContent)
+
+    if(operation == "minus") {
+      currentValue -= 1
+    } else {
+      currentValue += 1
+    }
+    if (currentValue < 1) {
+      return number.textContent = 1
+    } else {
+      return number.textContent = currentValue
+    }
+  }
+
+  const eventListener = () => {
+    counterButtons.forEach(button => {
+      button.addEventListener("click", (e) => {
+        const operation = e.currentTarget.dataset.counterButton
+        executeOperation(operation)
+        
+      })
+    })
+  }
+
+  const counterButtons = getItems("[data-counter-button]")
+  eventListener()
 }
